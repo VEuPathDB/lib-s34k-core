f47a841be5beac554488c6f2274729f7bcb5f197
Elizabeth <foxcapade@gmail.com>
2022-04-27 10:39:32 -0400

    update according to api change

--- src/main/kotlin/org/veupathdb/lib/s3/s34k/core/fields/query_params/BasicS3QueryParamsMutable.kt
+++ src/main/kotlin/org/veupathdb/lib/s3/s34k/core/fields/query_params/BasicS3QueryParamsMutable.kt
@@ -4,15 +4,15 @@ import org.veupathdb.lib.s3.s34k.fields.query_params.S3QueryParamsMutable
 
 open class BasicS3QueryParamsMutable : S3QueryParamsMutable, BasicS3QueryParams() {
 
-  override fun addQueryParam(header: String, vararg values: String) =
-    addVararg(header, *values)
+  override fun addQueryParam(param: String, vararg values: String) =
+    addVararg(param, *values)
 
-  override fun addQueryParam(header: String, values: Iterable<String>) =
-    addIterable(header, values)
+  override fun addQueryParam(param: String, values: Iterable<String>) =
+    addIterable(param, values)
 
-  override fun addQueryParams(headers: Map<String, Iterable<String>>) =
-    headers.forEach { (k, v) -> addQueryParam(k, v) }
+  override fun addQueryParams(params: Map<String, Iterable<String>>) =
+    params.forEach { (k, v) -> addQueryParam(k, v) }
 
-  override fun addQueryParams(vararg headers: Pair<String, String>) =
-    headers.forEach { (k, v) -> addQueryParam(k, v) }
+  override fun addQueryParams(vararg params: Pair<String, String>) =
+    params.forEach { (k, v) -> addQueryParam(k, v) }
 }
--- src/main/kotlin/org/veupathdb/lib/s3/s34k/core/requests/object/BasicS3ObjectDeleteParams.kt
+++ src/main/kotlin/org/veupathdb/lib/s3/s34k/core/requests/object/BasicS3ObjectDeleteParams.kt
@@ -4,7 +4,6 @@ import org.veupathdb.lib.s3.s34k.fields.headers.S3HeadersMutable
 import org.veupathdb.lib.s3.s34k.fields.query_params.S3QueryParamsMutable
 import org.veupathdb.lib.s3.s34k.requests.`object`.S3ObjectDeleteParams
 
-// TODO: Document me
 open class BasicS3ObjectDeleteParams : S3ObjectDeleteParams, BasicS3ObjectParams {
 
   override var callback: ((deleted: Boolean) -> Unit)?
@@ -18,15 +17,18 @@ open class BasicS3ObjectDeleteParams : S3ObjectDeleteParams, BasicS3ObjectParams
     callback:  ((deleted: Boolean) -> Unit)? = null,
   ) : super(path, region) {
     this.callback = callback
+    this.recursive = recursive
   }
 
   internal constructor(
     path:        String?,
     region:      String?,
+    recursive:   Boolean = false,
     callback:    ((deleted: Boolean) -> Unit)?,
     headers: S3HeadersMutable,
     queryParams: S3QueryParamsMutable
   ) : super(path, region, headers, queryParams) {
     this.callback = callback
+    this.recursive = recursive
   }
 }
